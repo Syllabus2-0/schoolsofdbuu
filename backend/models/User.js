2d0e21ab2fd4c4311a6e158c0067cfe6d85f6226
deepaksingh1227 <divyanthakur856@gmail.com>
2026-04-16 11:47:16 +0530

    try to add signup

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const roles = ["SuperAdmin", "Dean", "HOD", "Faculty"];
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: roles, default: "Faculty" },
+  },
+  { timestamps: true },
+);
+
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
